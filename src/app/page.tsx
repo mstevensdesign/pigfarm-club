e86e8af0b81c8acbe35921f0f892f9c5c517eeac
Mike Stevens <mike@mikestevens.design>
2024-06-25 03:08:33 -0400

    placeholder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h2>PigFarm Club</h2>;
+  return (
+    <>
+      <div className="flex h-screen w-screen flex-col items-center justify-center bg-yellow-200">
+        <Image
+          className="transition-all hover:scale-125"
+          src="/taco-smile.png"
+          alt="Taco Placeholder"
+          width={180}
+          height={37}
+          priority
+        />
+        <h2 className="text-3xl font-bold text-green-500">PigFarm Club</h2>
+      </div>
+    </>
+  );
 }
