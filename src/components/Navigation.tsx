05a6134ba8a90e6020e9377b87cc6f8c00b739df
Mike Stevens <mike@mikestevens.design>
2024-09-25 12:21:27 -0400

    make overwatch logo a separate file

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import overwatchLogo from "/logos/overwatch-logo.svg";
+import { OverwatchLogo } from "./OverwatchLogo";
 import { useState } from "react";
 import {
   Dialog,
@@ -28,34 +28,6 @@ import {
   PhoneIcon,
   PlayCircleIcon,
 } from "@heroicons/react/20/solid";
-
-const OverwatchLogo = (props: any) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    xmlSpace="preserve"
-    imageRendering="optimizeQuality"
-    shapeRendering="geometricPrecision"
-    style={{
-      enableBackground: "new 0 0 1581.6 1581.7",
-    }}
-    textRendering="geometricPrecision"
-    viewBox="0 0 1581.6 1581.7"
-    {...props}
-  >
-    <path
-      d="m1285.9 174.2-149.4 173.3c-202.2-156.1-488.1-160.2-691.5 0L295.6 174.2c287.2-232.1 702.6-232.4 990.3 0z"
-      style={{
-        fill: "#fa9c1e",
-      }}
-    />
-    <path
-      d="M244.7 218.8c3.2-2.9 6.1-6.2 9.6-8.9 49.7 57.7 99.4 115.3 149 173-1.4 2.2-3.5 3.8-5.3 5.7C264.4 520 204.3 705 237.6 891.5c11 61.2 31.9 120.7 62.9 174.6l308-298.3 152.8-328.9c2.3 166.3-.4 332.9.1 499.3l-309.3 301.2c190.8 148.5 491 153.6 679.2-1.5L823.9 938.2c-7-159.3.1-337.2.3-498.6l152.6 328.2 305.2 295.7c2.7-3.7 4.4-7.9 6.7-11.9 15.1-28.8 27.7-58.9 37.6-89.8 63.4-199 11.2-419.5-137.2-567.7-3.6-3.8-7.8-7.1-11-11.2 48.9-58.2 99.4-115.6 149.1-173.1 33.1 30.3 63.4 63.8 90.7 99.4 187.4 244.1 215.9 574.6 74.7 846.6-20.4 39.4-44.4 76.9-71.2 112.3-50.6 67.4-112.5 126.2-182 174-291.3 200.5-685.5 185.1-957.5-45.7-44.8-37.6-85.5-80.1-120.8-126.8-241-317.8-210.4-772.7 83.6-1050.8z"
-      style={{
-        fill: "#4a4c4e",
-      }}
-    />
-  </svg>
-);
 
 const products = [
   {
