6719bc23fa416760b57c0258ccd06fa147f9ff12
Mike Stevens <mike@mikestevens.design>
2024-09-25 17:43:30 -0400

    change title sizes

--- src/app/roster/page.tsx
+++ src/app/roster/page.tsx
@@ -5,7 +5,7 @@ type Props = {};
 const Roster = (props: Props) => {
   return (
     <div className="flex h-[calc(100vh-74px)] w-screen flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold text-green-500">Roster</h1>
+      <h1 className="text-5xl font-bold text-green-500">Roster</h1>
     </div>
   );
 };
