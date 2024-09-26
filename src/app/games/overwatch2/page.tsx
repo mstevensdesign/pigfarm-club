d7616414159f281d8fb2d99e380231b4d36d6a82
Mike Stevens <mike@mikestevens.design>
2024-09-26 12:53:19 -0400

    add react player youtube clip

--- src/app/games/overwatch2/page.tsx
+++ src/app/games/overwatch2/page.tsx
@@ -1,12 +1,18 @@
+"use client";
+
 import React from "react";
+import ReactPlayer from "react-player/youtube";
 
 type Props = {};
 
 const Overwatch2 = (props: Props) => {
   return (
-    <div className="flex h-[calc(100vh-74px)] w-screen flex-col items-center justify-center">
-      <h1 className="text-center">Overwatch 2</h1>
-    </div>
+    <>
+      <div className="flex h-[calc(100vh-74px)] w-screen flex-col items-center justify-center">
+        <h1 className="text-center">Overwatch 2</h1>
+        <ReactPlayer url="https://www.youtube.com/watch?v=Smoz9J3ZSts" />
+      </div>
+    </>
   );
 };
 
