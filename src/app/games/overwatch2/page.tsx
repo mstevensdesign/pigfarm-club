95b200330596977f950585e8cda2af918151eeca
Mike Stevens <mikestevensdesign@gmail.com>
2024-09-28 19:05:51 -0400

    hydration fix

--- src/app/games/overwatch2/page.tsx
+++ src/app/games/overwatch2/page.tsx
@@ -2,19 +2,31 @@
 
 import React from "react";
 import ReactPlayer from "react-player/youtube";
+import { useEffect, useState } from "react";
 
 type Props = {};
 
 const Overwatch2 = (props: Props) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+
+    // component will unmount
+    return () => {
+      setIsMounted(false);
+    };
+  }, []);
   return (
     <>
-      <h1 className="text-center">Overwatch 2</h1>
-      <div className="relative">
-        <ReactPlayer
-          className="absolute left-0 top-0"
-          url="https://www.youtube.com/watch?v=Smoz9J3ZSts"
-        />
-      </div>
+      {isMounted && (
+        <div>
+          <ReactPlayer
+            url="https://www.youtube.com/watch?v=Smoz9J3ZSts"
+            width="100%"
+          />
+        </div>
+      )}
     </>
   );
 };
