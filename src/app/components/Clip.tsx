fbd2475b3779ecc41ed95b658f5072ae10a76a76
Mike Stevens <mikestevensdesign@gmail.com>
2024-09-29 15:26:05 -0400

    move component mount code again to try to clean up the render

--- src/app/components/Clip.tsx
+++ src/app/components/Clip.tsx
@@ -21,24 +21,24 @@ const Clip = (props: Props) => {
 
   return (
     <>
-      <div className="m-5 max-w-5xl">
-        <div className="relative pt-[56.25%]">
-          <h2 className="absolute bottom-0 left-0 z-10 text-sm md:text-2xl">
-            {props.title}
-          </h2>
-          <h2 className="absolute bottom-0 right-0 z-10 text-sm md:text-2xl">
-            {props.description}
-          </h2>
-          {isMounted && (
+      {isMounted && (
+        <div className="m-5 max-w-5xl">
+          <div className="relative pt-[56.25%]">
+            <h2 className="absolute bottom-0 left-0 z-10 text-sm md:text-2xl">
+              {props.title}
+            </h2>
+            <h2 className="absolute bottom-0 right-0 z-10 text-sm md:text-2xl">
+              {props.description}
+            </h2>
             <ReactPlayer
               url={props.url}
               className="absolute left-0 top-0"
               width="100%"
               height="100%"
             />
-          )}
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
