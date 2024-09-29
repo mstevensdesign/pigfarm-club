125727226823b2f84279e0e58ea213aa2393cb5a
Mike Stevens <mikestevensdesign@gmail.com>
2024-09-29 15:07:40 -0400

    fix font size on clip component

--- src/app/components/Clip.tsx
+++ src/app/components/Clip.tsx
@@ -21,10 +21,12 @@ const Clip = (props: Props) => {
 
   return (
     <>
-      <div className="m-5">
+      <div className="m-5 max-w-5xl">
         <div className="relative pt-[56.25%]">
-          <h2 className="absolute bottom-0 left-0 z-10">{props.title}</h2>
-          <h2 className="absolute bottom-0 right-0 z-10">
+          <h2 className="absolute bottom-0 left-0 z-10 text-sm md:text-2xl">
+            {props.title}
+          </h2>
+          <h2 className="absolute bottom-0 right-0 z-10 text-sm md:text-2xl">
             {props.description}
           </h2>
           {isMounted && (
--- src/app/games/overwatch2/page.tsx
+++ src/app/games/overwatch2/page.tsx
@@ -8,21 +8,23 @@ const Overwatch2 = (props: Props) => {
   return (
     <>
       <h2 className="mt-5 text-center text-lg">Overwatch 2</h2>
-      <Clip
-        url="https://www.youtube.com/watch?v=Smoz9J3ZSts"
-        title="Rip BANX"
-        description="This is a description"
-      />
-      <Clip
-        url="https://www.youtube.com/watch?v=Smoz9J3ZSts"
-        title="Rip BANX"
-        description="This is a description"
-      />
-      <Clip
-        url="https://www.youtube.com/watch?v=Smoz9J3ZSts"
-        title="Rip BANX"
-        description="This is a description"
-      />
+      <div className="">
+        <Clip
+          url="https://www.youtube.com/watch?v=Smoz9J3ZSts"
+          title="Rip BANX"
+          description="This is a description"
+        />
+        <Clip
+          url="https://www.youtube.com/watch?v=Smoz9J3ZSts"
+          title="Rip BANX"
+          description="This is a description"
+        />
+        <Clip
+          url="https://www.youtube.com/watch?v=Smoz9J3ZSts"
+          title="Rip BANX"
+          description="This is a description"
+        />
+      </div>
     </>
   );
 };
