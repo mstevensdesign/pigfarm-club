e083f3671d16fcc81712c7efdf2a3fd7c98789ed
Mike Stevens <mikestevensdesign@gmail.com>
2024-09-28 20:20:58 -0400

    responsive react player

--- src/app/games/overwatch2/page.tsx
+++ src/app/games/overwatch2/page.tsx
@@ -20,10 +20,12 @@ const Overwatch2 = (props: Props) => {
   return (
     <>
       {isMounted && (
-        <div>
+        <div className="relative pt-[56.25%]">
           <ReactPlayer
             url="https://www.youtube.com/watch?v=Smoz9J3ZSts"
+            className="absolute left-0 top-0"
             width="100%"
+            height="100%"
           />
         </div>
       )}
