45152aa541c1e68073cbf139c0695fe8e6c2fa97
Mike Stevens <mike@mikestevens.design>
2024-09-29 12:29:38 -0400

    center landing image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-      <Image
-        className="transition-all hover:scale-125"
-        src="/taco-smile.png"
-        alt="Taco Placeholder"
-        width={180}
-        height={37}
-        priority
-      />
-      <h2 className="text-5xl font-bold text-green-500">PigFarm Club</h2>
-    </>
+    <div>
+      <div className="flex h-[calc(100vh-74px*2)] w-screen flex-col items-center justify-center bg-yellow-400">
+        <Image
+          className="transition-all hover:scale-125"
+          src="/taco-smile.png"
+          alt="Taco Placeholder"
+          width={180}
+          height={37}
+          priority
+        />
+        <h2 className="text-5xl font-bold text-green-500">PigFarm Club</h2>
+      </div>
+    </div>
     // <>
     //   <div className="flex h-[calc(100vh-74px)] w-screen flex-col items-center justify-center bg-yellow-400">
     //     <Image
