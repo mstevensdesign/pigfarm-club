a55935461a58b9b074cf4d96c76e7d7d7c8183b9
Mike Stevens <mike@mikestevens.design>
2024-09-30 04:58:13 -0400

    fix header margin

--- src/app/games/overwatch2/page.tsx
+++ src/app/games/overwatch2/page.tsx
@@ -7,7 +7,7 @@ type Props = {};
 const Overwatch2 = (props: Props) => {
   return (
     <>
-      <h2 className="mt-5 text-center text-lg">Overwatch 2</h2>
+      <h2 className="my-3 text-center text-lg">Overwatch 2</h2>
       <div className="">
         <Clip
           url="https://www.youtube.com/watch?v=Smoz9J3ZSts"
