b26c6160b79fde2853e719f9433f936cd2dabc0a
Mike Stevens <mike@mikestevens.design>
2024-10-01 23:54:53 -0400

    change padding of clip grid

--- src/app/games/overwatch2/page.tsx
+++ src/app/games/overwatch2/page.tsx
@@ -10,7 +10,7 @@ const Overwatch2 = (props: Props) => {
       <h2 className="my-3 text-center text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
         Overwatch 2
       </h2>
-      <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 xl:grid-cols-3">
+      <div className="grid grid-cols-1 gap-x-2 gap-y-4 p-2 lg:grid-cols-2 xl:grid-cols-3">
         <Clip
           url="https://www.youtube.com/watch?v=Smoz9J3ZSts"
           title="Rip BANX"
