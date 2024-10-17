5ac15725e618c2e601cca86d54f9b8201184d78e
Mike Stevens <mike@mikestevens.design>
2024-10-16 20:56:35 -0400

    add getclipsbytags function

--- src/app/clips/page.tsx
+++ src/app/clips/page.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import ClipGrid from "../components/ClipGrid";
-import getClips from "../utils/utils";
+import { getClips, getClipsByTag } from "../utils/utils";
 
 type Props = {};
 
 const ClipsPage = async (props: Props) => {
   const clips_ow2 = await getClips("ow2");
+  const clips_by_tag = await getClipsByTag("ow2", ["walkblind"]);
 
   return (
     <div className="">
       <ClipGrid clips={clips_ow2} game="Overwatch 2" />
-      <ClipGrid clips={clips_ow2} game="Test" />
+      <ClipGrid clips={clips_by_tag} game="Test" />
     </div>
   );
 };
--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -1,7 +1,13 @@
 import { sql } from "@vercel/postgres";
 
-export default async function getClips(game: string) {
+export async function getClips(game: string) {
   const { rows } =
     await sql`SELECT * from CLIPS where game = ${game} ORDER BY id ASC`;
+  return rows;
+}
+
+export async function getClipsByTag(game: string, tags: string[]) {
+  const { rows } =
+    await sql`SELECT * FROM clips WHERE game = ${game} AND 'walkblind' = ANY(players)`;
   return rows;
 }
