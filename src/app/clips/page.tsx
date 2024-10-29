2a4719572a0e01f8c599c2e89b63e58d4dfb5b9b
Mike Stevens <mike@mikestevens.design>
2024-10-29 12:51:27 -0400

    update getclipbyname

--- src/app/clips/page.tsx
+++ src/app/clips/page.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ClipGrid from "../components/ClipGrid";
-import { getClips, getClipsByTag } from "../utils/utils";
+import { getClips, getClipsByTag, getClipsByGame } from "../utils/utils";
 import { MagnifyingGlassIcon } from "@heroicons/react/16/solid";
 import FilterModal from "../components/FilterModal";
 
 type Props = {};
 
 const ClipsPage = async (props: Props) => {
-  const clips = await getClips();
+  const clips = await getClipsByGame(1);
 
   return (
     <div className="">
--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -4,9 +4,9 @@ export async function getClips() {
   const { rows } = await sql`SELECT * from CLIPS ORDER BY id ASC`;
   return rows;
 }
-export async function getClipsByGame(game: string) {
+export async function getClipsByGame(game_id: number) {
   const { rows } =
-    await sql`SELECT * from CLIPS where game = ${game} ORDER BY id ASC`;
+    await sql`SELECT * from CLIPS where game_id = ${game_id} ORDER BY id ASC`;
   return rows;
 }
 
