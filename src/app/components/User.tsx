9f8823174c83ac6d85a2eef5641eb2b863adb5d3
Mike Stevens <mikestevensdesign@gmail.com>
2024-10-30 15:59:10 -0400

    adad other user properties to user page

--- src/app/components/User.tsx
+++ src/app/components/User.tsx
@@ -6,10 +6,19 @@ const User = (props: Props) => {
   return (
     <div>
       <p>User ID: {props.user?.id}</p>
-      <p>User Name: {props.user?.first_name}</p>
+      <p>First Name: {props.user?.first_name}</p>
+      <p>Last Name: {props.user?.last_name}</p>
+      <p>Email: {props.user?.email}</p>
+      <p>Display Name: {props.user?.display_name}</p>
       <img className="max-w-96" src={props.user?.profile_url} alt="avatar" />
+      {/* <Description /> */}
     </div>
   );
+};
+
+type DescriptionProps = {};
+const Description = (props: DescriptionProps) => {
+  return <p>description</p>;
 };
 
 export default User;
