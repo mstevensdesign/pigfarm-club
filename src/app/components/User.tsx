a840e2bf8b63eecacf9d8962ec1067a1faa35ba8
Mike Stevens <mikestevensdesign@gmail.com>
2024-10-30 00:22:19 -0400

    stash more user profile code

--- src/app/components/User.tsx
+++ src/app/components/User.tsx
@@ -1,17 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { getUserById } from "../utils/utils";
 
 type Props = {
   user_id: number;
 };
 
-const User = async (props: Props) => {
-  const user = await getUserById(props.user_id);
-  console.log(user);
+const User = (props: Props) => {
   return (
     <div>
       <h1>USER</h1>
-      <p>{props.user_id}</p>
     </div>
   );
 };
--- src/app/roster/[user_id]/page.tsx
+++ src/app/roster/[user_id]/page.tsx
@@ -3,16 +3,29 @@
 import { useParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import User from "@/app/components/User";
+import { getUserById } from "@/app/utils/utils";
 
 type Props = {};
 
 const UserPage = (props: Props) => {
-  // const user_id = Number(useParams().user_id);
-  // console.log(user_id);
+  const user_id = Number(useParams().user_id);
+  console.log(user_id);
+
+  const [user, setUser] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const userData = await getUserById(1);
+      console.log("userData", userData);
+      // setUser(userData);
+    };
+
+    fetchUser();
+  }, []);
 
   return (
     <div className="grid h-[calc(100vh-74px)] place-content-center">
-      <User user_id={2} />
+      {/* <User user_id={user_id} /> */}
     </div>
   );
 };
