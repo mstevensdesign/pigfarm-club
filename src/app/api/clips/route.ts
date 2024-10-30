070d305a0276df163d0189fbc5a46b680c9c7d45
Mike Stevens <mikestevensdesign@gmail.com>
2024-10-30 14:18:51 -0400

    add search params for get clips by game id

--- src/app/api/clips/route.ts
+++ src/app/api/clips/route.ts
@@ -1,7 +1,34 @@
 import { sql } from "@vercel/postgres";
+import { type NextRequest } from "next/server";
 
-export async function GET() {
-  const data = { user_id: 1 };
-  const { rows } = await sql`SELECT * from CLIPS ORDER BY id ASC`;
-  return Response.json(rows);
+// export async function GET() {
+//   const data = { user_id: 1 };
+//   const { rows } = await sql`SELECT * from CLIPS ORDER BY id ASC`;
+//   return Response.json(rows);
+// }
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const game_id = searchParams.get("game_id");
+  // query is "hello" for /api/search?query=hello
+  if (game_id) {
+    const { rows } = await sql`SELECT clips.url as clip_url,
+clips.title as clip_title,
+clips.description as clip_description,
+clips.date as clip_date,
+clips.id as clip_id,
+games.title as game_title,
+users.display_name as user_display_name,
+users.id as user_id,
+users.profile_url as profile_url
+FROM clips
+JOIN games ON clips.game_id = games.id
+JOIN users ON clips.user_id = users.id
+WHERE clips.game_id = ${game_id}
+ORDER BY clips.id ASC`;
+    return Response.json(rows);
+  } else {
+    const { rows } = await sql`SELECT * from CLIPS ORDER BY id ASC`;
+    return Response.json(rows);
+  }
 }
