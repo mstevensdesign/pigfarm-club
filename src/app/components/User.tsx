6dcd2851e8d3b9b168336c211d5ae5de19cf8668
Mike Stevens <mikestevensdesign@gmail.com>
2024-11-07 22:49:10 -0500

    stash the roster members page layout

--- src/app/components/User.tsx
+++ src/app/components/User.tsx
@@ -17,64 +17,87 @@ type Props = {
   user: any;
 };
 
+// const User = (props: Props) => {
+//   return (
+//     <div>
+//       <div className="flex gap-10">
+//         {/* <h1>MEMBER PAGE</h1> */}
+//         <img
+//           className="w-32 rounded-md object-cover"
+//           src={props.user?.profile_url}
+//           alt="avatar"
+//         />
+//         <div className="flex flex-col justify-between gap-5">
+//           <div className="">
+//             <div className={`${londrina.className} text-h6 text-green`}>
+//               {props.user?.display_name}
+//             </div>
+//             <div className="">{`${props.user?.pronouns}, ${props.user?.dob}`}</div>
+//           </div>
+//           <div className="">
+//             <div className={`${maven.className}`}>
+//               {props.user?.description}
+//             </div>
+//           </div>
+//         </div>
+//         {/* <Description /> */}
+//       </div>
+//       <div className="flex justify-center gap-20 pb-2 pt-10 text-black">
+//         <svg
+//           xmlns="http://www.w3.org/2000/svg"
+//           fill="none"
+//           viewBox="0 0 24 24"
+//           strokeWidth={1.5}
+//           stroke="currentColor"
+//           className="size-6"
+//         >
+//           <path
+//             strokeLinecap="round"
+//             strokeLinejoin="round"
+//             d="M3.375 19.5h17.25m-17.25 0a1.125 1.125 0 0 1-1.125-1.125M3.375 19.5h7.5c.621 0 1.125-.504 1.125-1.125m-9.75 0V5.625m0 12.75v-1.5c0-.621.504-1.125 1.125-1.125m18.375 2.625V5.625m0 12.75c0 .621-.504 1.125-1.125 1.125m1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125m0 3.75h-7.5A1.125 1.125 0 0 1 12 18.375m9.75-12.75c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125m19.5 0v1.5c0 .621-.504 1.125-1.125 1.125M2.25 5.625v1.5c0 .621.504 1.125 1.125 1.125m0 0h17.25m-17.25 0h7.5c.621 0 1.125.504 1.125 1.125M3.375 8.25c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125m17.25-3.75h-7.5c-.621 0-1.125.504-1.125 1.125m8.625-1.125c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h7.5m-7.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125M12 10.875v-1.5m0 1.5c0 .621-.504 1.125-1.125 1.125M12 10.875c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125M13.125 12h7.5m-7.5 0c-.621 0-1.125.504-1.125 1.125M20.625 12c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h7.5M12 14.625v-1.5m0 1.5c0 .621-.504 1.125-1.125 1.125M12 14.625c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125m0 1.5v-1.5m0 0c0-.621.504-1.125 1.125-1.125m0 0h7.5"
+//           />
+//         </svg>
+
+//         <svg
+//           xmlns="http://www.w3.org/2000/svg"
+//           fill="none"
+//           viewBox="0 0 24 24"
+//           strokeWidth={1.5}
+//           stroke="currentColor"
+//           className="size-6"
+//         >
+//           <path
+//             strokeLinecap="round"
+//             strokeLinejoin="round"
+//             d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
+//           />
+//         </svg>
+//       </div>
+//       <div className="mx-auto">
+//         <hr className="border-gray-500" />
+//       </div>
+//     </div>
+//   );
+// };
+
 const User = (props: Props) => {
   return (
-    <div>
-      <div className="flex gap-10">
-        {/* <h1>MEMBER PAGE</h1> */}
-        <img
-          className="w-32 rounded-md object-cover"
-          src={props.user?.profile_url}
-          alt="avatar"
-        />
-        <div className="flex flex-col justify-between gap-5">
-          <div className="">
-            <div className={`${londrina.className} text-h6 text-green`}>
-              {props.user?.display_name}
-            </div>
-            <div className="">{`${props.user?.pronouns}, ${props.user?.dob}`}</div>
-          </div>
-          <div className="">
-            <div className={`${maven.className}`}>
-              {props.user?.description}
-            </div>
-          </div>
+    <div className="flex flex-col items-center lg:flex-row">
+      <img
+        className="w-36 rounded-md object-cover"
+        src={props.user?.profile_url}
+        alt="avatar"
+      />
+      <div className="flex flex-col items-center bg-pink-300 lg:items-start">
+        <div className={`${londrina.className} text-h6 text-green`}>
+          {props.user?.display_name}
         </div>
-        {/* <Description /> */}
-      </div>
-      <div className="flex justify-center gap-20 pb-2 pt-10 text-black">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="size-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M3.375 19.5h17.25m-17.25 0a1.125 1.125 0 0 1-1.125-1.125M3.375 19.5h7.5c.621 0 1.125-.504 1.125-1.125m-9.75 0V5.625m0 12.75v-1.5c0-.621.504-1.125 1.125-1.125m18.375 2.625V5.625m0 12.75c0 .621-.504 1.125-1.125 1.125m1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125m0 3.75h-7.5A1.125 1.125 0 0 1 12 18.375m9.75-12.75c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125m19.5 0v1.5c0 .621-.504 1.125-1.125 1.125M2.25 5.625v1.5c0 .621.504 1.125 1.125 1.125m0 0h17.25m-17.25 0h7.5c.621 0 1.125.504 1.125 1.125M3.375 8.25c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125m17.25-3.75h-7.5c-.621 0-1.125.504-1.125 1.125m8.625-1.125c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h7.5m-7.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125M12 10.875v-1.5m0 1.5c0 .621-.504 1.125-1.125 1.125M12 10.875c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125M13.125 12h7.5m-7.5 0c-.621 0-1.125.504-1.125 1.125M20.625 12c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h7.5M12 14.625v-1.5m0 1.5c0 .621-.504 1.125-1.125 1.125M12 14.625c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125m0 1.5v-1.5m0 0c0-.621.504-1.125 1.125-1.125m0 0h7.5"
-          />
-        </svg>
+        <div className="flex flex-col items-center lg:items-start">
+          <div className="">{`${props.user?.pronouns}`}</div>
+          <div className="">{`${props.user?.dob}`}</div>
+        </div>
 
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="size-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
-          />
-        </svg>
-      </div>
-      <div className="mx-auto">
-        <hr className="border-gray-500" />
+        <div className={`${maven.className}`}>{props.user?.description}</div>
       </div>
     </div>
   );
