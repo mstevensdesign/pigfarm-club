d5c0cee03675efb5f467d0872331ba8343f8bf89
Mike Stevens <mikestevensdesign@gmail.com>
2024-11-11 18:51:29 -0500

    add get clips by user_id api route with switch statement

--- src/app/api/clips/route.ts
+++ src/app/api/clips/route.ts
@@ -9,8 +9,38 @@ import { type NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
+  console.log(searchParams);
   const game_id = searchParams.get("game_id");
   // query is "hello" for /api/search?query=hello
+  switch (searchParams.keys().next().value) {
+    case "game_id":
+      const { rows } = await sql`SELECT clips.url as clip_url,
+clips.title as clip_title,
+clips.description as clip_description,
+clips.date as clip_date,
+clips.id as clip_id,
+games.title as game_title,
+users.display_name as user_display_name,
+users.id as user_id,
+users.profile_url as profile_url
+FROM clips
+JOIN games ON clips.game_id = games.id
+JOIN users ON clips.user_id = users.id
+WHERE clips.game_id = ${game_id}
+ORDER BY clips.id ASC`;
+      return Response.json(rows);
+      break;
+    case "user_id":
+      const user_id = searchParams.get("user_id");
+      const clipRows =
+        await sql`SELECT * from CLIPS WHERE user_id = ${user_id} ORDER BY id ASC`;
+      return Response.json(clipRows.rows);
+
+    default:
+      console.log("default");
+      const defaultRows = await sql`SELECT * from CLIPS ORDER BY id ASC`;
+      return Response.json(defaultRows.rows);
+  }
   if (game_id) {
     const { rows } = await sql`SELECT clips.url as clip_url,
 clips.title as clip_title,
