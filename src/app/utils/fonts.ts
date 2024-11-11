4403f4146a2b89c4835c8e8e4ee2429baaa6e877
Mike Stevens <mikestevensdesign@gmail.com>
2024-11-11 15:20:04 -0500

    set up fonts utility file

--- src/app/utils/fonts.ts
+++ src/app/utils/fonts.ts
@@ -1,1 +1,13 @@
+import { Maven_Pro, Londrina_Solid } from "next/font/google";
 
+export const maven = Maven_Pro({
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const londrina = Londrina_Solid({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
