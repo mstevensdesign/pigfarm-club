6f43f191c56d53903a41bb388aab5822532b7fcd
Mike Stevens <mikestevensdesign@gmail.com>
2024-11-12 19:17:15 -0500

    refactor home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,48 @@
 import Image from "next/image";
-import { Londrina_Solid } from "next/font/google";
-const londrina = Londrina_Solid({
-  weight: "400",
-  subsets: ["latin"],
-  display: "swap",
-});
+import { londrina } from "./utils/fonts";
+
 export default function Home() {
   return (
-    <div>
-      <div className="flex h-[calc(100vh-74px*3)] w-screen flex-col items-center justify-center">
-        <Image
-          className="transition-all hover:scale-125"
-          src="/taco-bon.svg"
-          alt="Taco Placeholder"
-          width={270}
-          height={0}
-          priority
-        />
-        <h2 className={`text-h4 ${londrina.className} text-green -mt-16`}>
-          PigFarm Club
-        </h2>
-        {/* <Image
-          src={"/arrow-down-circle.svg"}
-          alt="down arrow"
-          width={42}
-          height={0}
-        /> */}
-      </div>
-    </div>
+    <Container>
+      <Logo />
+      <Title />
+    </Container>
   );
 }
+
+import { ReactNode } from "react";
+
+const Container = ({ children }: { children: ReactNode }) => {
+  return (
+    <>
+      <div className="flex h-[calc(100vh-74px*3)] w-screen flex-col items-center justify-center">
+        {children}
+      </div>
+    </>
+  );
+};
+
+const Logo = () => {
+  return (
+    <>
+      <Image
+        className="transition-all hover:scale-125"
+        src="/taco-bon.svg"
+        alt="Taco Placeholder"
+        width={270}
+        height={0}
+        priority
+      />
+    </>
+  );
+};
+
+const Title = () => {
+  return (
+    <>
+      <h2 className={`text-h4 ${londrina.className} -mt-16 text-green`}>
+        PigFarm Club
+      </h2>
+    </>
+  );
+};
