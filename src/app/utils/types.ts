42121b000c8fa807236e39cb2cdb0656ef74e010
Mike Stevens <mikestevensdesign@gmail.com>
2024-11-12 21:20:33 -0500

    create TClip type

--- src/app/utils/types.ts
+++ src/app/utils/types.ts
@@ -6,3 +6,14 @@ export type TUser = {
   dob: string;
   description: string;
 };
+
+export type TClip = {
+  id: number;
+  title: string;
+  description: string;
+  url: string;
+  thumbnail_url: string;
+  created_at: string;
+  updated_at: string;
+  user: TUser;
+};
