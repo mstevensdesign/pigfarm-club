7726657d16478cd761ff2d11ff0c5617318ee668
Mike Stevens <mikestevensdesign@gmail.com>
2024-11-12 20:31:45 -0500

    refactor clips page

--- src/app/clips/page.tsx
+++ src/app/clips/page.tsx
@@ -15,23 +15,55 @@ const ClipsPage = async (props: Props) => {
 
   return (
     <>
-      <div className="mb-4 mt-1 flex justify-center bg-pink-400 px-6">
-        <div className="relative w-full max-w-md bg-indigo-300">
-          <input
-            type="text"
-            className="w-full rounded-md border p-2 pl-10 pr-4 focus:outline-none focus:ring-2 focus:ring-slate-500"
-            placeholder="Search clips..."
-          />
+      <Container>
+        <SearchContainer>
+          <SearchInput />
           <MagnifyingGlassIcon className="absolute left-3 top-2.5 h-5 w-5 text-gray-400" />
-          <button className="absolute right-3 top-2 rounded-md text-white focus:outline-none focus:ring-2 focus:ring-slate-500">
-            {/* Filter */}
-            <FilterModal clips={clips} />
-          </button>
-        </div>
-      </div>
+          <FilterButton clips={clips} />
+        </SearchContainer>
+      </Container>
       <ClipGrid clips={clips} title="Overwatch 2" />
     </>
   );
 };
 
 export default ClipsPage;
+
+const Container = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <div className="mb-4 mt-1 flex justify-center px-6">{children}</div>
+    </>
+  );
+};
+
+const SearchContainer = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <div className="relative w-full max-w-md">{children}</div>
+    </>
+  );
+};
+
+const SearchInput = () => {
+  return (
+    <>
+      <input
+        type="text"
+        className="w-full rounded-md border p-2 pl-10 pr-4 focus:outline-none focus:ring-2 focus:ring-slate-500"
+        placeholder="Search clips..."
+      />
+    </>
+  );
+};
+
+const FilterButton = (clips: any) => {
+  return (
+    <>
+      <button className="absolute right-3 top-2 rounded-md text-white focus:outline-none focus:ring-2 focus:ring-slate-500">
+        {/* Filter */}
+        <FilterModal clips={clips} />
+      </button>
+    </>
+  );
+};
