23d53db75b794a05b6475ea7654a0ddf6aeabda8
Mike Stevens <mikestevensdesign@gmail.com>
2024-11-16 17:02:35 -0500

    fix next config to load images from vercel

--- src/app/roster/page.tsx
+++ src/app/roster/page.tsx
@@ -99,11 +99,11 @@ const Roster = (props: Props) => {
           role="list"
           className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-14 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 xl:grid-cols-4"
         >
-          {people.map((person) => (
+          {data.map((person: any) => (
             <li key={person.name}>
               <Image
                 alt=""
-                src={person.imageUrl}
+                src={person.profile_url}
                 className="aspect-[14/13] w-full rounded-2xl object-cover"
                 width={200}
                 height={200}
@@ -118,6 +118,40 @@ const Roster = (props: Props) => {
         </ul>
       </div>
     </div>
+
+    // <div className="py-24 sm:py-32">
+    //   <div className="mx-auto max-w-7xl px-6 lg:px-8">
+    //     <div className="mx-auto max-w-2xl lg:mx-0">
+    //       <h2 className="text-3xl font-bold tracking-tight sm:text-4xl">
+    //         Our Team
+    //       </h2>
+    //       <p className="mt-6 text-lg leading-8">
+    //         We are a bunch of nerds who love gaming and building cool stuff.
+    //       </p>
+    //     </div>
+    //     <ul
+    //       role="list"
+    //       className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-14 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 xl:grid-cols-4"
+    //     >
+    //       {people.map((person) => (
+    //         <li key={person.name}>
+    //           <Image
+    //             alt=""
+    //             src={person.imageUrl}
+    //             className="aspect-[14/13] w-full rounded-2xl object-cover"
+    //             width={200}
+    //             height={200}
+    //           />
+    //           <h3 className="mt-2 text-lg font-semibold leading-8 tracking-tight">
+    //             {person.name}
+    //           </h3>
+    //           <p className="">{person.role}</p>
+    //           <p className="">{person.location}</p>
+    //         </li>
+    //       ))}
+    //     </ul>
+    //   </div>
+    // </div>
 
     // <div className="flex h-[calc(100vh-74px)] w-screen flex-col items-center justify-center">
     //   <h1 className="text-5xl font-bold text-green-500">Roster</h1>
