c2fe361f0e76bb349032c59c72d597301f0d2610
Mike Stevens <mikestevensdesign@gmail.com>
2024-11-21 02:56:05 -0500

    add very basic styling to the clip detail card

--- src/app/clips/[clip_id]/page.tsx
+++ src/app/clips/[clip_id]/page.tsx
@@ -25,26 +25,30 @@ const ClipDetailPage = () => {
   console.log("Clip Data", clipData);
 
   return (
-    <>
+    <div className="container mx-auto p-4">
       {clipLoading ? (
         <div>Loading...</div>
       ) : clipError ? (
         <div>Error: {clipError.message}</div>
       ) : (
-        <Clip
-          key={clipData.id}
-          id={clipData.id}
-          url={clipData.url}
-          title={clipData.title}
-          description={clipData.description}
-          date={clipData.date}
-          user_display_name={clipData.user_display_name}
-          game_title={clipData.game_title}
-          user_profile_url={clipData.user_profile_url}
-          user_id={clipData.user_id}
-        />
+        <div className="flex justify-center">
+          <div className="w-full max-w-2xl">
+            <Clip
+              key={clipData.id}
+              id={clipData.id}
+              url={clipData.url}
+              title={clipData.title}
+              description={clipData.description}
+              date={clipData.date}
+              user_display_name={clipData.user_display_name}
+              game_title={clipData.game_title}
+              user_profile_url={clipData.user_profile_url}
+              user_id={clipData.user_id}
+            />
+          </div>
+        </div>
       )}
-    </>
+    </div>
   );
 };
 export default ClipDetailPage;
