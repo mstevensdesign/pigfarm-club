43ea9c5b73d5d82ce8bfc1e59828a322d01f23f4
Mike Stevens <mikestevensdesign@gmail.com>
2024-11-21 14:32:55 -0500

    add responsive style to roster page

--- src/app/roster/page.tsx
+++ src/app/roster/page.tsx
@@ -35,21 +35,22 @@ const Roster = () => {
   return (
     <>
       <h4 className="text-center">Our Team</h4>
-      <ul role="list" className="">
+      <ul
+        role="list"
+        className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+      >
         {data.map((person: TUser) => (
-          <li key={person.id} className="group mb-20 flex">
+          <li key={person.id} className="group flex flex-col items-center">
             <Link href={`/roster/${person.id}`}>
-              <div className="relative bg-pink-400">
+              <div className="relative h-48 w-48 transform overflow-hidden rounded-lg bg-pink-400 transition-transform duration-300 group-hover:scale-105 sm:h-56 sm:w-56 md:h-64 md:w-64 lg:h-72 lg:w-72">
                 <Image
                   alt={person.first_name}
                   src={person.profile_url}
-                  className=""
-                  width={200}
-                  height={200}
+                  className="object-cover"
+                  layout="fill"
                 />
                 <div className="absolute bottom-0 flex w-full flex-col bg-black bg-opacity-55 text-white">
-                  <div className="self-center">{person.display_name}</div>
-                  {/* <div className="h-5 w-1/2 self-end rounded-md bg-white"></div> */}
+                  <div className="self-center py-2">{person.display_name}</div>
                 </div>
               </div>
             </Link>
