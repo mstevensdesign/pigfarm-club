af39bb681bfdf4808c006c34b1e8072193dbe57d
Mike Stevens <mikestevensdesign@gmail.com>
2024-12-02 19:25:05 -0500

    add force dynamic to user route

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,5 +1,7 @@
 import { sql } from "@vercel/postgres";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   const { rows } = await sql`SELECT * FROM users ORDER BY id ASC`;
   return Response.json(rows);
